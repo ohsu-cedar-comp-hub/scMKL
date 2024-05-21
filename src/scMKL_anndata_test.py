851900e0850bcc5dc426dcde0969420004137e39
skupp32 <skupp@uoregon.edu>
2024-05-21 15:02:15 -0700

    Partial changes made locally to be overwritten

--- src/scMKL_anndata_test.py
+++ src/scMKL_anndata_test.py
@@ -1,3 +1,4 @@
+from turtle import distance
 import numpy as np
 import scMKL_src_anndata as src
 from scipy.sparse import load_npz
@@ -10,36 +11,38 @@ import tracemalloc
 tracemalloc.start()
 
 parser = argparse.ArgumentParser(description='Unimodal classification of single cell data with hallmark prior information')
-parser.add_argument('-d', '--dataset', help = 'Which dataset to classify', choices = ['prostate',  'MCF7', 'T47D', 'lymphoma'], type = str)
+# parser.add_argument('-d', '--dataset', help = 'Which dataset to classify', choices = ['prostate',  'MCF7', 'T47D', 'lymphoma'], type = str)
 parser.add_argument('-a', '--assay', help = 'Which assay to use', type = str, choices = ['rna', 'atac', 'gene_scores'])
-parser.add_argument('-m', '--distance_metric', help = 'Which of the scipy distance functions to use for estimating sigma', type = str)
 parser.add_argument('-r', '--replication', help = 'Which replication to use', type = int, default = 1)
 args = parser.parse_args()
 
-dataset = args.dataset
+# dataset = args.dataset
 assay = args.assay
 replication = args.replication
-distance_metric = args.distance_metric
 
-X = load_npz(f'/home/groups/CEDAR/kupp/scMKL/data/scMM/{dataset}/{dataset}_{assay.upper()}_scipy.npz')
-feature_names = np.load(f'/home/groups/CEDAR/kupp/scMKL/data/scMM/{dataset}/{dataset}_{assay.upper()}_feature_names.npy', allow_pickle= True)
-cell_labels = np.load(f'/home/groups/CEDAR/kupp/scMKL/data/scMM/{dataset}/{dataset}_cell_metadata.npy', allow_pickle= True)
+dataset = 'MCF7'
 
-with open(f'/home/groups/CEDAR/kupp/scMKL/data/scMM/{dataset}/{dataset}_groupings.pkl', 'rb') as fin:
+X = load_npz(f'example/data/{dataset}_{assay.upper()}_X.npz')
+feature_names = np.load(f'example/data/{dataset}_{assay.upper()}_feature_names.npy', allow_pickle= True)
+cell_labels = np.load(f'example/data/{dataset}_cell_labels.npy', allow_pickle= True)
+
+with open(f'example/data/{dataset}_feature_groupings.pkl', 'rb') as fin:
     groupings = pickle.load(fin)
 
 if assay == 'atac':
     feature_type = 'peaks'
     kernel_func = 'Laplacian'
     dtype = 'binary'
+    distance_metric = 'cityblock'
 else:
     feature_type = 'genes'
     kernel_func = 'Gaussian'
     dtype = 'counts'
+    distance_metric = 'euclidean'
 
 group_dict = {}
-for pathway in groupings['hallmark'].keys():
-    group_dict[pathway] = groupings['hallmark'][pathway][feature_type]
+for pathway in groupings.keys():
+    group_dict[pathway] = groupings[pathway][feature_type]
 
 D = int(np.sqrt(len(cell_labels)) * np.log(np.log(len(cell_labels))))
 
@@ -51,35 +54,32 @@ print('Estimating Sigma', flush = True)
 adata = src.Estimate_Sigma(adata, distance_metric = distance_metric, n_features = 200)
 
 print('Optimizing Sigma', flush = True)
-# adata = src.Optimize_Sigma(adata, kernel_type = kernel_func)
+adata = src.Optimize_Sigma(adata, kernel_type = kernel_func)
 
 print('Calculating Z', flush = True)
 adata = src.Calculate_Z(adata, kernel_type = kernel_func, n_features = 5000)
 
-# print(adata.uns['Z_train'][0:3, 0:3])
+alpha_list = np.round(np.linspace(1.9,0.1,10), 2)
+if dataset == 'prostate':
+    alpha_list = np.round(np.linspace(3.6, 0.1, 10), 2)
 
+metric_dict = {}
+selected_pathways = {}
+group_norms = {}
+group_names = list(group_dict.keys())
+predicted = {}
+auroc_array = np.zeros(alpha_list.shape)
+start = time.time()
 
-# alpha_list = np.round(np.linspace(1.9,0.1,10), 2)
-# if dataset == 'prostate':
-#     alpha_list = np.round(np.linspace(3.6, 0.1, 10), 2)
-
-# metric_dict = {}
-# selected_pathways = {}
-# group_norms = {}
-# group_names = list(group_dict.keys())
-# predicted = {}
-# auroc_array = np.zeros(alpha_list.shape)
-# start = time.time()
-
-# for i, alpha in enumerate(alpha_list):
+for i, alpha in enumerate(alpha_list):
     
-#     print(f'  Evaluating model. Alpha: {alpha}', flush = True)
+    print(f'  Evaluating model. Alpha: {alpha}', flush = True)
 
-#     adata = src.Train_Model(adata, group_size= 2*D, alpha = alpha)
-#     predicted[alpha], metric_dict[alpha] = src.Predict(adata, metrics = ['AUROC','F1-Score', 'Accuracy', 'Precision', 'Recall'])
-#     selected_pathways[alpha] = src.Find_Selected_Pathways(adata)
-#     group_norms[alpha] = [np.linalg.norm(adata.uns['model'].coef_[i * 2 * D: (i + 1) * 2 * D - 1]) for i in np.arange(len(group_names))]
-# end = time.time()
+    adata = src.Train_Model(adata, group_size= 2*D, alpha = alpha)
+    predicted[alpha], metric_dict[alpha] = src.Predict(adata, metrics = ['AUROC','F1-Score', 'Accuracy', 'Precision', 'Recall'])
+    selected_pathways[alpha] = src.Find_Selected_Pathways(adata)
+    group_norms[alpha] = [np.linalg.norm(adata.uns['model'].coef_[i * 2 * D: (i + 1) * 2 * D - 1]) for i in np.arange(len(group_names))]
+end = time.time()
 
 # results = {}
 # results['Metrics'] = metric_dict
@@ -90,9 +90,10 @@ adata = src.Calculate_Z(adata, kernel_type = kernel_func, n_features = 5000)
 # results['Inference_time'] = end - start
 # results['RAM_usage'] = f'{tracemalloc.get_traced_memory()[1] / 1e9} GB'
 
-# print(f'Memory Usage: {tracemalloc.get_traced_memory()[1] / 1e9} GB')
-# tracemalloc.stop()
+print(f'Memory Usage: {tracemalloc.get_traced_memory()[1] / 1e9} GB')
+tracemalloc.stop()
 
+print(metric_dict)
 
 # # output_dir = f'/home/groups/CEDAR/kupp/scMKL/results/scMM/{dataset}/python_MAKL/result_files/unimodal_hallmark'
 # # file_name = f'{dataset}_{assay}_hallmark_{distance_metric}_dist_replication_{replication}.pkl'
