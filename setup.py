ac12d471d82b0d5ba333fb4a82e36c9c054c8e63
ivango17 <ivangordon17@georgefox.edu>
2024-10-25 15:08:23 -0700

    fixed package dependency issue and noted python version range in README

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name = 'scmkl',
-    version = '0.1.2',
+    version = '0.1.3',
     description = "Single-cell analysis using Multiple Kernel Learning",
     long_description = long_description,
     long_description_content_type = 'text/markdown',
@@ -14,7 +14,7 @@ setup(
     author_email = 'kupp@ohsu.edu, vangordi@ohsu.edu, ak@ohsu.edu',
     url = 'https://github.com/ohsu-cedar-comp-hub/scMKL/tree/main',
     packages = find_packages(),
-    python_requires = '>=3.11.1',
+    python_requires = '>=3.11.1, <=3.13',
     install_requires = [
         'wheel==0.41.2',
         'anndata==0.10.8',
