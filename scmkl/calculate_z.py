Fixed csc_matrix issues with calculating TFIDF per pathway, added assert statement to check reduction technique, and bug fixes with SVD calculation.

scmkl/calculate_z.py
@@ -42,6 +42,8 @@ def _process_data(X_train, X_test = None, scale_data = True, return_dense = True
         X_train, X_test- Numpy arrays with the process train/test data respectively.
     '''
 
+    if scipy.sparse.issparse(X_train):
+        X_train = scipy.sparse.csc_array(X_train)
 
     if X_test is None:
             X_test = X_train[:1,:] # Creates dummy matrix to for the sake of calculation without increasing computational time
@@ -74,11 +76,13 @@ def _process_data(X_train, X_test = None, scale_data = True, return_dense = True
         X_train = (X_train - train_means) / train_sds
         X_test = (X_test - train_means) / train_sds
 
+
     if return_dense and scipy.sparse.issparse(X_train):
         X_train = X_train.toarray()
         X_test = X_test.toarray()
 
-    if orig_test == None:
+
+    if orig_test is None:
         return X_train
     else:
         return X_train, X_test
@@ -118,8 +122,8 @@ def calculate_z(adata, n_features = 5000) -> ad.AnnData:
     D = adata.uns['D']
 
     # Create Arrays to store concatenated group Z.  Each group of features will have a corresponding entry in each array
-    Z_train = np.zeros((len(adata.uns['train_indices']), 2 * adata.uns['D'] * N_pathway))
-    Z_test = np.zeros((len(adata.uns['test_indices']), 2 * adata.uns['D'] * N_pathway))
+    Z_train = np.zeros((len(adata.uns['train_indices']), 2 * adata.uns['D'] * N_pathway)).astype(np.float16)
+    Z_test = np.zeros((len(adata.uns['test_indices']), 2 * adata.uns['D'] * N_pathway)).astype(np.float16)
 
     # Loop over each of the groups and creating Z for each
     for m, group_features in enumerate(adata.uns['group_dict'].values()):
@@ -143,15 +147,15 @@ def calculate_z(adata, n_features = 5000) -> ad.AnnData:
         # If given data_type is 'counts' (like RNA) will log scale and z-score the data
         X_train, X_test = _process_data(X_train = X_train, X_test = X_test, scale_data = adata.uns['scale_data'], return_dense = False)            
 
-        if adata.uns['reduction'] == 'svd':
+        if adata.uns['reduction'].lower() == 'svd':
 
             SVD_func = TruncatedSVD(n_components = np.min([50, X_train.shape[1]]), random_state = 1)
             
             # Remove first component as it corresponds with sequencing depth
-            X_train = SVD_func.fit_transform(csr_array(X_train))[:, 1:]
-            X_test = SVD_func.transform(csr_array(X_test))[:, 1:]
+            X_train = SVD_func.fit_transform(scipy.sparse.csr_array(X_train))[:, 1:]
+            X_test = SVD_func.transform(scipy.sparse.csr_array(X_test))[:, 1:]
 
-        elif adata.uns['reduction'] == 'pca':
+        elif adata.uns['reduction'].lower() == 'pca':
             PCA_func = PCA(n_components = np.min([50, X_train.shape[1]]), random_state = 1)
 
             X_train = PCA_func.fit_transform(np.asarray(X_train))
@@ -163,8 +167,8 @@ def calculate_z(adata, n_features = 5000) -> ad.AnnData:
             X_test = X_test @ adata.uns['seed_obj'].choice([0,1], p = [0.02, 0.98], size = X_test.shape[1] * 50).reshape((X_train.shape[1], 50))
 
         if scipy.sparse.issparse(X_train):
-            X_train = X_train.toarray()
-            X_test = X_test.toarray()
+            X_train = X_train.toarray().astype(np.float16)
+            X_test = X_test.toarray().astype(np.float16)
 
         #Extract pre-calculated sigma used for approximating kernel
         adjusted_sigma = adata.uns['sigma'][m]

scmkl/create_adata.py
@@ -331,6 +331,7 @@ def create_adata(X, feature_names: np.ndarray, cell_labels: np.ndarray,
         assert len(np.unique(cell_labels)) == 2, 'cell_labels must contain 2 classes'
     assert isinstance(D, int) and D > 0 or D is None, 'D must be a positive integer'
     assert kernel_type.lower() in ['gaussian', 'laplacian', 'cauchy'], 'Given kernel type not implemented. Gaussian, Laplacian, and Cauchy are the acceptable types.'
+    assert reduction in [None, 'SVD', 'svd', 'PCA', 'pca'], 'Given reduction technique not implemented. PCA and SVA are the supported methods.'
 
     X, feature_names, group_dict = _filter_features(X, feature_names, group_dict, remove_features)
 
@@ -345,7 +346,7 @@ def create_adata(X, feature_names: np.ndarray, cell_labels: np.ndarray,
     adata.uns['D'] = D if D is not None else calculate_d(adata.shape[0])
     adata.uns['kernel_type'] = kernel_type
     adata.uns['distance_metric'] = distance_metric
-    adata.uns['reduction'] = reduction
+    adata.uns['reduction'] = reduction if isinstance(reduction, str) else 'None'
     adata.uns['tfidf'] = tfidf
 
     if (split_data is None):

scmkl/estimate_sigma.py
@@ -35,7 +35,7 @@ def estimate_sigma(adata, n_features = 5000):
     sigma_list = []
 
     # Loop over every group in group_dict
-    for group_features in adata.uns['group_dict'].values():
+    for m, group_features in enumerate(adata.uns['group_dict'].values()):
 
         # Select only features within that group and downsample for scalability
         num_group_features = len(group_features)
@@ -43,24 +43,25 @@ def estimate_sigma(adata, n_features = 5000):
 
         # Use on the train data to estimate sigma
         X_train = adata[adata.uns['train_indices'], group_features].X
+
         X_train = _process_data(X_train = X_train, scale_data = adata.uns['scale_data'], return_dense = False)
-        
+
         # Sample cells because distance calculation are costly and can be approximated
         distance_indices = adata.uns['seed_obj'].choice(np.arange(X_train.shape[0]), np.min((2000, X_train.shape[0])))
 
         if adata.uns['tfidf']:
             X_train = _tfidf(X_train, mode = 'normalize')
 
-        if adata.uns['reduction'] == 'svd':
+        if adata.uns['reduction'].lower() == 'svd':
 
             SVD_func = TruncatedSVD(n_components = np.min([50, X_train.shape[1]]), random_state = 1)
-            X_train = SVD_func.fit_transform(csr_array(X_train[distance_indices,:]))[:,1:]
+            X_train = SVD_func.fit_transform(scipy.sparse.csr_array(X_train[distance_indices,:]))[:,1:]
 
-        elif adata.uns['pca'] == 'pca':
+        elif adata.uns['reduction'].lower() == 'pca':
             PCA_func = PCA(n_components = np.min([50, X_train.shape[1]]), random_state = 1)
             X_train = PCA_func.fit_transform(np.asarray(X_train[distance_indices,:]))
 
-        elif adata.uns['reduction'] == 'linear':
+        elif adata.uns['reduction'].lower() == 'linear':
 
             X_train = X_train[distance_indices] @ adata.uns['seed_obj'].choice([0,1], [0.02, 0.98]).reshape((len(distance_indices), 50))
 

scmkl/tfidf_normalize.py
@@ -16,12 +16,12 @@ def _tfidf(X, mode = 'filter'):
     '''
 
     assert mode in ['filter', 'normalize'], 'mode must be "filter" or "normalize".'
-    
+
     if scipy.sparse.issparse(X):
         tf = scipy.sparse.csc_array(X)
-        doc_freq = np.array(np.sum(X > 0, axis=0)).flatten()
+        doc_freq = np.array(np.sum(tf > 0, axis=0)).reshape(-1)
     else:
-        tf = X
+        tf = np.asarray(X)
         doc_freq = np.sum(X > 0, axis=0)
 
     idf = np.log1p((1 + X.shape[0]) / (1 + doc_freq))
@@ -39,7 +39,7 @@ def _tfidf_train_test(X_train, X_test):
     if scipy.sparse.issparse(X_train):
         tf_train = scipy.sparse.csc_array(X_train)
         tf_test = scipy.sparse.csc_array(X_test)
-        doc_freq = np.array(np.sum(X_train > 0, axis=0)).flatten()
+        doc_freq = np.array(np.sum(X_train > 0, axis=0)).reshape(-1)
     else:
         tf_train = X_train
         tf_test = X_test