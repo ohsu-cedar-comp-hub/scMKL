fixed bug in macro f1_score calculation and updated v0.3.0 dist with fix

scmkl/one_v_rest.py
@@ -199,7 +199,8 @@ def one_v_rest(adatas : list, names : list, alpha_list : np.ndarray,
     # Getting final predictions
     alpha = np.min(alpha_list)
     prob_table, pred_class, low_conf = _prob_table(results, alpha)
-    macro_f1 = f1_score(cell_labels, pred_class, average='macro')
+    macro_f1 = f1_score(cell_labels[adata.uns['test_indices']], 
+                        pred_class, average='macro')
 
     results['Probability_table'] = prob_table
     results['Predicted_class'] = pred_class